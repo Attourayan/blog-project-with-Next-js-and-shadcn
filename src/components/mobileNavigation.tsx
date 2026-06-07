2c928c7c6ef68cf88a7b70c5bdb47d8bc86e9d57
attou-med <attourayan@gmail.com>
2026-06-07 19:30:01 +0100

    finaly projcet build 2

--- src/components/mobileNavigation.tsx
+++ src/components/mobileNavigation.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import {
   Sheet,
   SheetClose,
@@ -11,25 +10,22 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import { ThemToggle } from "./themToggle";
 import { navItems } from "@/lib/constants";
 import Link from "next/link";
 import { useState } from "react";
 
 const MobileNavigation = () => {
-
-const [isOpen,setIsOpen] = useState(false);
-
-
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="flex  space-x-5 md:hidden ">
       <Sheet open={isOpen}>
-         <ThemToggle   />
-        <SheetTrigger asChild >
+        <ThemToggle />
+        <SheetTrigger asChild>
           <button
-          onClick={()=>setIsOpen(true)}
+            onClick={() => setIsOpen(true)}
             type="button"
             className="text-muted-foreground hover:text-foreground focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary rounded-sm p-1"
             aria-controls="mobile-menu"
@@ -39,14 +35,24 @@ const [isOpen,setIsOpen] = useState(false);
             <Menu />
           </button>
         </SheetTrigger>
-        <SheetContent   >
+        <SheetContent>
           <SheetTitle></SheetTitle>
-        <div className="flex flex-col   items-left mt-8 p-5 space-y-10">
-          {navItems.map((item)=>
-            <Link key={item.name
-            } href={item.href} onClick={()=>setIsOpen(false)}  className="text-sm font-sans text-muted-foreground hover:text-primary  hover:underline font-bold transition-colors duration-300">{item.name}</Link>
-          )}
-        </div>
+          <SheetClose className="flex justify-end px-4" onClick={() => setIsOpen(false)}>
+            <X />
+          </SheetClose>
+
+          <div className="flex flex-col  items-left mt-2 p-5 space-y-10">
+            {navItems.map((item) => (
+              <Link
+                key={item.name}
+                href={item.href}
+                onClick={() => setIsOpen(false)}
+                className="text-sm font-sans text-muted-foreground hover:text-primary  hover:underline font-bold transition-colors duration-300"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
         </SheetContent>
       </Sheet>
     </div>
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -49,7 +49,7 @@ function SheetContent({
   className,
   children,
   side = "right",
-  showCloseButton = true,
+  showCloseButton = false,
   ...props
 }: React.ComponentProps<typeof SheetPrimitive.Content> & {
   side?: "top" | "right" | "bottom" | "left"
